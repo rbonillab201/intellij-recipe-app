998ceb7ebcdef7f52ee43df098d3f3046c09028a
rbonillab201 <renatio@gmail.com>
2020-10-13 16:15:38 -0600

    Se actualizará el proyecto, prueba.

--- src/main/java/net/cuscatlan/intellijrecipeapp/controllers/IndexController.java
+++ src/main/java/net/cuscatlan/intellijrecipeapp/controllers/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"","/"})
 public class IndexController {
 
-    @GetMapping({"","/","/index"})
+    @GetMapping({"","/","/index","/index.hmtl"})
     String myIndex(Model model){
         model.addAttribute("title","Recipe Apps");
         return "index";
