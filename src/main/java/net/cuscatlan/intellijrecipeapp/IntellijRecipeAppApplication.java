11db96f4c332fe0800483ec1737f222f225209cf
Renato Bonilla <renatio@gmail.com>
2020-10-13 16:07:52 -0600

    Se actualizará el proyecto.

---
+++ src/main/java/net/cuscatlan/intellijrecipeapp/IntellijRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.intellijrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cuscatlan/intellijrecipeapp/IntellijRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.intellijrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
